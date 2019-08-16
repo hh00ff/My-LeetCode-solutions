850a78e2bb5df1a23d261e41a62a44a86d4b3662
hh00ff <hufengqing123@gmail.com>
2019-08-16 14:34:49 +0800

    Add LC0042.

--- LC0042_TrappingRainWater.cpp
+++ LC0042_TrappingRainWater.cpp
@@ -43,9 +43,42 @@ public:
     }
 };
 
+class Test {
+public:
+    Test() {
+        sol = Solution();
+    }
+
+    void test1() {
+        std::vector<int> input1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int expects = 6;
+
+        check("Test 1", input1, expects);
+    }
+
+private:
+    void check(const char* testname, std::vector<int>& input1, const int& expects) {
+        bool tst_pass = true;
+        auto res = sol.trap(input1);
+
+        if(res != expects) {
+            tst_pass = false;
+        }
+
+        if(tst_pass){
+            std::cout << testname << ": " << "pass" << std::endl;
+        }
+        else {
+            std::cout << testname << ": " << "fail" << std::endl;
+        }
+    }
+
+    Solution sol;
+};
+
 int main(int argc, char** argv) {
-    auto sol = Solution();
-    std::vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    std::cout << sol.trap(height) << std::endl;
+    Test tst = Test();
+    tst.test1();
     return 0;
 }
+
